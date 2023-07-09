2ba5a4a08b9fcb7c04f7a290a78b09b10bae185f
AnasYusuf14 <100074475+AnasYusuf14@users.noreply.github.com>
2023-07-09 15:02:23 +0300

    Update CeaserCipher.py

--- CeaserCipher.py
+++ CeaserCipher.py
@@ -12,30 +12,24 @@ a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8
 8b         88 88       d8 88       88 8PP" 88          
 "8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
  `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
-              88                                             
+              88                                             encode
               88           
 ''')
 
-direction = input("Enter 'encode' if you want to do encrypt , Enter 'decode' if you want to do 'decrypt' \n ").lower()
+type = input("Enter 'encode' if you want to do encrypt , Enter 'decode' if you want to do 'decrypt' \n ").lower()
 text = input("Your message:\n").lower()
 shift_number = int(input("Enter the shift number: \n"))
 #########################################################
-#Encrypt Function:
-def encrypt_fun(str,shift):
-    cipher_text = ''.join(chr((ord(letter) - 97 + shift) % 26 + 97) for letter in str)
-    print(f"Encrypted text is {cipher_text}")  
-###########################################################  
-#Decrypt Function:
-def decrypt_fun(str,shift):
-    for letter in str:
+
+def ceasar(str , shift , direction):
+    if direction == "encode":
+        cipher_text = ''.join(chr((ord(letter) - 97 + shift) % 26 + 97) for letter in str)
+        print(f"Encrypted text is {cipher_text}")
+    elif direction == "decode":
         decrypted_text = ''.join(chr((ord(letter) - 97 - shift) % 26 + 97) for letter in str)
-    print(f"Decrypted text is {decrypted_text}")
-#############################################################  
-if direction == "encode":
-    encrypt_fun(str = text,shift = shift_number)
-elif direction == "decode":
-    decrypt_fun(str = text , shift = shift_number)
-else:
-    print("Enter only 'encode' or 'decode' !!")    
+        print(f"Decrypted text is {decrypted_text}")
+    else:
+        print("Enter only 'encode' or 'decode' !!") 
+ceasar(str = text ,shift=shift_number ,direction= type)
 
 
